bf434244ed0f60aeb26242452aed0c4a04b349fb
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2025-03-23 08:45:09 +0800

    CS3247-33 Add enemy sprites.

--- Source/CS3247_Project/GameplayAbilities/AttributeSet/BasicAttributeSet.cpp
+++ Source/CS3247_Project/GameplayAbilities/AttributeSet/BasicAttributeSet.cpp
@@ -4,7 +4,7 @@
 #include "BasicAttributeSet.h"
 
 void UBasicAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) {
-`	if (Attribute == this->GetHealthAttribute()) {
+	if (Attribute == this->GetHealthAttribute()) {
 		NewValue = FMath::Clamp(NewValue, 0.0f, this->GetMaxHealth());
 	} else if (Attribute == this->GetMaxHealthAttribute() || Attribute == this->GetDefenceAttribute()) {
 		NewValue = FMath::Max(NewValue, 0.0f);
